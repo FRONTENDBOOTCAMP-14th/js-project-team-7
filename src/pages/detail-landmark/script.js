b07b3f138914794950bba6c5208fe1c3a3da75c4
chajiiiii <j.cha1708@gmail.com>
2025-07-24 14:20:24 +0900

    [feat] : null 체크용 코드 추가

--- src/pages/detail-landmark/script.js
+++ src/pages/detail-landmark/script.js
@@ -2,12 +2,14 @@
 {
   const contents = document.querySelector('.contents');
 
-  contents.addEventListener('click', (e) => {
-    const button = e.target.closest('.accordion .landmark_name_button');
-    const photo = e.target.closest('.photo_container');
-    if (!button && !photo) return;
+  if (contents) {
+    contents.addEventListener('click', (e) => {
+      const button = e.target.closest('.accordion .landmark_name_button');
+      const photo = e.target.closest('.photo_container');
+      if (!button && !photo) return;
 
-    const clickedListItem = (button || photo).closest('.accordion');
-    clickedListItem.classList.toggle('is_opened');
-  });
+      const clickedListItem = (button || photo).closest('.accordion');
+      clickedListItem.classList.toggle('is_opened');
+    });
+  }
 }
